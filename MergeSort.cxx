e9427b64d8220a9c58f52c3c9489c64d394309b1
Muhammad Rizki <rizki003131@gmail.com>
2026-04-02 18:54:42 +0700

    Add int main

--- MergeSort.cxx
+++ MergeSort.cxx
@@ -40,5 +40,57 @@ void mergeSort(int low, int high){
     mergeSort(low, mid);
     mergeSort(mid + 1, high);
     
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+}
+
+while (i <= mid && j <= high){
+    if (arr[i] <= arr[j])
+    {
+        B[k] = arr[i];
+        i++;
+    }
+    else
+    {
+        B[k] = arr[j];
+        j++;
+    }
+    k++;
+}
+
+while (i <= mid){
+        B[k] = arr[i];
+        i++;
+        k++;
+}
+
+for (int x = low; x <= high; x++)
+{
+        arr[x] = B[x];
+}
+
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n- 1);
+    output();
+}
+
+
+        
+        
 
 
